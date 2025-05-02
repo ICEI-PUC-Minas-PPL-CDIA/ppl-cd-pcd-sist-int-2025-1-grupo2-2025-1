4d1558b5229ca40bc6dd970e63f46147090953be
Gabriel Nascimento <gabrielchaves0308@GMAIL.COM>
2025-05-01 22:49:46 -0300

    correcao_limpezadedados_2pergunta #93

--- src/code_data_cleanest/base_auxiliares/limpezadedados_2pergunta.py
+++ src/code_data_cleanest/base_auxiliares/limpezadedados_2pergunta.py
@@ -15,5 +15,36 @@ class MTEDataProcessor:
             '212305', '212315', '212320',            # Analistas de dados
             '142515', '142520', '142525', '142535',  # Gerentes de TI
             '221105', '221205',                      # Outros 
-
+profissionais relevantes
+        ]
+        
+        # Mapeamentos para transformação de dados
+        self.column_mapping = {
+            'CBO Ocupação 2002': 'cbo',
+            'Vl Remun Média Nom': 'salario_medio_nominal',
+            'Vl Remun Dezembro Nom': 'salario_dezembro_nominal',
+            'Sexo Trabalhador': 'genero_cod',
+            'Raça Cor': 'raca_cod',
+            'IBGE Subsetor': 'cnae_cod',
+            'UF': 'uf_cod',
+            'Escolaridade após 2005': 'escolaridade_cod',
+            'Idade': 'idade',
+        }
+        
+        self.gender_mapping = {
+            1: 'Masculino', 
+            2: 'Feminino', 
+            -1: 'Nao Identificado'
+        }
+        
+        self.race_mapping = {
+            1: 'Indigena', 
+            2: 'Branca', 
+            4: 'Preta', 
+            8: 'Amarela', 
+            9: 'Parda',
+            -1: 'Nao Identificado', 
+            6: 'Nao Informado'
+        }
+        
        
